46d70ad6f063702988f36a1dfd43765af3dbf4d3
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2024-04-04 02:17:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //your code here!
+// Get the list element
+const list = document.getElementById("infi-list");
 
+// Add 10 list items by default
+for (let i = 1; i <= 10; i++) {
+    const li = document.createElement("li");
+    li.textContent = "Item " + i;
+    list.appendChild(li);
+}
+
+function addMoreItems() {
+    for (let i = 1; i <= 2; i++) {
+        const li = document.createElement("li");
+        li.textContent = "Item " + (list.children.length + i);
+        list.appendChild(li);
+    }
+}
+
+// Event listener to check if the user has scrolled to the bottom of the list
+list.addEventListener("scroll", function() {
+    if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+        addMoreItems();
+    }
+});
